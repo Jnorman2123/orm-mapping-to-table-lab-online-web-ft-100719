5ca3567162b3429652e4c682b4376ecb0c44c1ce
Jonathan Norman <jnorman2123@gmail.com>
2019-11-07 01:11:43 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,4 +3,10 @@ class Student
   #  with DB[:conn]
   attr_accessor :name, :grade
   attr_reader id:
+
+  def initialize(id = nil, name, grade)
+    @id = id
+    @name = name
+    @grade = grade
+  end
 end

e34854a117f9bbecc0ad42ebc9442755c1829085
Jonathan Norman <jnorman2123@gmail.com>
2019-11-07 01:21:01 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -34,6 +34,6 @@ class Student
     VALUES (?, ?)
     SQL
     DB[:conn].execute(sql, self.name, self.grade)
-    @id = DB[:conn].execute("SELECT last_from_rowid()")[0][0]
+    @id = DB[:conn].execute("SELECT last_from_rowid() FROM students")[0][0]
   end
 end
